2948f144e40dc761d115cb7407e1a9123fc03dd5
wkumanuvong <waleerat@msn.com>
2024-08-15 13:20:46 +0000

    lab python day3

---
+++ sys-admin_2.py
@@ -0,0 +1,13 @@
+import subprocess
+
+#ubprocess.run(["ls","-l","README.md"])
+
+# command="uname"
+# commandArgument="-a"
+# print(f'Gathering system information with command: {command} {commandArgument}')
+# subprocess.run([command,commandArgument])
+
+command="ps"
+commandArgument="-x"
+print(f'Gathering active process information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
